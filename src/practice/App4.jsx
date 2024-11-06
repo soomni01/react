8f0542ee0653510f9e703d5c7718293d86374409
soomni01 <dsgh8933@naver.com>
2024-11-06 09:40:15 +0900

    jsx 작성법

--- src/practice/App4.jsx
+++ src/practice/App4.jsx
@@ -5,8 +5,8 @@ function App4(props) {
     <div>
       <h1 className="error">Lorem.</h1>
       <div>
-        <label htmlFor="check1"></label>
-        <input type="text" id="check1" />
+        <label htmlFor="check1">동의</label>
+        <input type="checkbox" id="check1" />
       </div>
       <hr />
       <h3>hello</h3>
