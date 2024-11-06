2373609db163e8102e6c9df5e42853bfc1b3a4c9
soomni01 <dsgh8933@naver.com>
2024-11-06 09:19:14 +0900

    rsf code snippet

---
+++ src/apps/MyApp3.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyApp3(props) {
+  return <div>hello app3</div>;
+}
+
+export default MyApp3;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import MyApp2 from "./apps/MyApp2.jsx";
+import MyApp3 from "./apps/MyApp3.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <MyApp2 />
+    <MyApp3 />
   </StrictMode>,
 );
