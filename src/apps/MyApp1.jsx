35fb787987d7f30addcff90fb75ba8194d43d2bd
soomni01 <dsgh8933@naver.com>
2024-11-06 10:42:26 +0900

    정리 추가

--- src/apps/MyApp1.jsx
+++ src/apps/MyApp1.jsx
@@ -16,3 +16,5 @@ export default MyApp1;
 
 // 리액트에서는 보통 JSX라는 문법을 사용해 UI를 작성
 // JSX는 JavaScript와 HTML을 결합한 형태인데, 실제로는 HTML 태그처럼 보임
+// 리액트는 JavaScript를 이용해 HTML을 동적으로 생성하고 갱신하는 방식
+// {}는 JavaScript 코드를 실행하는 곳, 브라우저는 최종적으로 리액트가 렌더링한 HTML을 표시
