5721c22230911f3ab186e46532280c2fe43654ee
soomni01 <dsgh8933@naver.com>
2024-11-07 14:01:19 +0900

    컴포넌트 배치

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App31 from "./apps/MyApp31.jsx";
+import App32 from "./apps/MyApp32.jsx";
 import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App31 />
+      <App32 />
     </Provider>
   </StrictMode>,
 );
