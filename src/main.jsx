dac89974b2737a81320a2e31741d443e739f9593
soomni01 <dsgh8933@naver.com>
2024-11-07 09:47:44 +0900

    input field

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App27 from "./apps/MyApp27.jsx";
+import App28 from "./apps/MyApp28.jsx";
 import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App27 />
+      <App28 />
     </Provider>
   </StrictMode>,
 );
