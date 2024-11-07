e74b655a88bf797ad721b6c366e9324b58efd10e
soomni01 <dsgh8933@naver.com>
2024-11-07 10:20:36 +0900

    checkbox

--- src/apps/MyApp28.jsx
+++ src/apps/MyApp28.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import { Input, Textarea } from "@chakra-ui/react";
 import { Field } from "../components/ui/field.jsx";
+import { Checkbox } from "../components/ui/checkbox.jsx";
+import { HiOutlinePlus } from "react-icons/hi2";
 
 function MyApp28(props) {
   return (
     <div style={{ margin: "10px" }}>
+      <h5>checkbox</h5>
+      <Checkbox icon={<HiOutlinePlus />}>동의</Checkbox>
+      <Checkbox variant="outline" checked>
+        {" "}
+        동의
+      </Checkbox>
+      <hr />
+      <Checkbox variant="solid"> 동의</Checkbox>
+      <hr />
+      <Checkbox variant="subtle"> 동의</Checkbox>
+      <hr />
+      <input type="checkbox" id="check1" />
+      <label htmlFor="check1">동의</label>
       <hr />
       <Textarea placeholder="자기소개 1000자 제한" variant="flushed"></Textarea>
       <hr />
