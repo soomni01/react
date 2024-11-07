387ec2c24a2ce5296f0194d812aa7d2488127191
soomni01 <dsgh8933@naver.com>
2024-11-07 11:52:31 +0900

    responsive design

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App30 from "./apps/MyApp30.jsx";
+import App31 from "./apps/MyApp31.jsx";
 import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App30 />
+      <App31 />
     </Provider>
   </StrictMode>,
 );
