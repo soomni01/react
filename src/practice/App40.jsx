dd31816369447142701c550bec9e8b88c9756869
soomni01 <dsgh8933@naver.com>
2024-11-08 09:42:46 +0900

    state 가  객체(object, list)면 새 객체를 만들어서(복사) 상태를 변경해야함

---
+++ src/practice/App40.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Button } from "../components/ui/button.jsx";
+
+function App40(props) {
+  const [numbers, setNumbers] = useState([1, 2, 3]);
+  function handleAddClick() {
+    const nextNumbers = [...numbers, numbers[numbers.length - 1] + 1];
+    setNumbers(nextNumbers);
+  }
+  function handleDeleteClick() {
+    const nextNumbers = [...numbers];
+    nextNumbers.pop();
+    setNumbers(nextNumbers);
+  }
+  return (
+    <div>
+      <Button onClick={handleAddClick}>추가</Button>
+      <Button onClick={handleDeleteClick}>삭제</Button>
+      <ul>
+        {numbers.map((num) => (
+          <li>{num}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App40;
