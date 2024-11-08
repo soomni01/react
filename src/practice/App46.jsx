3b1eb9cb80ca8aad942d287fdb9ca42d164413de
soomni01 <dsgh8933@naver.com>
2024-11-08 14:12:09 +0900

    state를 prop으로 넘기기

---
+++ src/practice/App46.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Field } from "../components/ui/field.jsx";
+import { Box, Input } from "@chakra-ui/react";
+
+function Comp({ text }) {
+  return (
+    <div>
+      <Box h={20} bg={"yellow.300"}>
+        {text}
+      </Box>
+    </div>
+  );
+}
+function App46(props) {
+  const [text, setText] = useState();
+  return (
+    <div>
+      <Field label={"자기소개"}>
+        <Input onKeyUp={(e) => setText(e.target.value)} />
+      </Field>
+      <Comp text={text} />
+    </div>
+  );
+}
+
+export default App46;
