a6065ec76b35b9fc38ab03c12ebd71c7ce8697c8
soomni01 <dsgh8933@naver.com>
2024-11-08 14:23:29 +0900

    prop dfiing

--- src/apps/MyApp47.jsx
+++ src/apps/MyApp47.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Field } from "../components/ui/field.jsx";
+import { Box, Input } from "@chakra-ui/react";
 
 function MyApp47(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <Field label={"소개"}>
+        <Input onChange={(e) => setText(e.target.value)} />
+      </Field>
+      <Comp text={text} />
+      <Comp2 message={text} />
+      <Comp3 message={text} />
+    </div>
+  );
+}
+function Comp4({ message }) {
+  return (
+    <Box>
+      <h3>comp4</h3>
+      <Box>{message}</Box>
+    </Box>
+  );
+}
+function Comp5({ message }) {
+  return (
+    <Box>
+      <h3>comp5</h3>
+      <Box>{message}</Box>
+    </Box>
+  );
+}
+function Comp3(props) {
+  return (
+    <>
+      <h3>comp3</h3>
+      <Comp4 message={props.message} />
+      <Comp5 {...props} />
+    </>
+  );
+}
+function Comp2({ message }) {
+  return <Box>{message}</Box>;
+}
+
+function Comp({ text }) {
+  return <Box>{text}</Box>;
 }
 
 export default MyApp47;
