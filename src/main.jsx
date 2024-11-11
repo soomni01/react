2db47c675bb048098e6e2a47a7d206fa6838a261
soomni01 <dsgh8933@naver.com>
2024-11-11 14:01:55 +0900

    promise

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp55 from "./apps/MyApp55.jsx";
+import MyApp57 from "./apps/MyApp57.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp55 />
+      <MyApp57 />
       <Toaster />
     </Provider>
   </StrictMode>,
