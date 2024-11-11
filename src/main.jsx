5eb526b6ab197419f289f8ea1f550c5d836abed1
soomni01 <dsgh8933@naver.com>
2024-11-11 15:19:58 +0900

    json 사용 예

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp57 from "./apps/MyApp57.jsx";
+import MyApp60 from "./apps/MyApp60.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp57 />
+      <MyApp60 />
       <Toaster />
     </Provider>
   </StrictMode>,
