272c60eac58df15b00478f9a4e236909fc35e570
soomni01 <dsgh8933@naver.com>
2024-11-11 11:41:19 +0900

    strict mode : 개발 시 컴포넌트 마운트 될 때 고의적으로 언마운트,마운트를 시도함

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp51 from "./apps/MyApp51.jsx";
+import MyApp55 from "./apps/MyApp55.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
-  <Provider>
-    <MyApp51 />
-    <Toaster />
-  </Provider>,
-  // </StrictMode>,
+  <StrictMode>
+    <Provider>
+      <MyApp55 />
+      <Toaster />
+    </Provider>
+  </StrictMode>,
 );
