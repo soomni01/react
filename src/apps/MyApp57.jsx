ebfbfd25512e23c2e9067b9ae627351cef7b1cb1
soomni01 <dsgh8933@naver.com>
2024-11-11 14:07:00 +0900

    request 보내기

---
+++ src/apps/MyApp57.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "../components/ui/button.jsx";
+
+function MyApp57(props) {
+  return (
+    <div>
+      {/* javascript 사용 */}
+      <Button onClick={() => {}}>get 요청</Button>
+      <Button onClick={() => {}}>post 요청</Button>
+      <hr />
+      {/* html 요소 이용 (a, form) */}
+      {/* get */}
+      <a href="/hi">to hi</a>
+      <hr />
+      <form action="/hello" method="get">
+        <button>전송</button>
+      </form>
+      <hr />
+      {/* post */}
+      <form action="/greeting" method="post">
+        <button>전송</button>
+      </form>
+    </div>
+  );
+}
+
+export default MyApp57;
