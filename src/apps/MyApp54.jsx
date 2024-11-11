f2c7d8e35b759b1a5dd385187ba9f7ef91f3a22b
soomni01 <dsgh8933@naver.com>
2024-11-11 10:52:50 +0900

    useEffect

---
+++ src/apps/MyApp54.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { Box, Input } from "@chakra-ui/react";
+import { Button } from "../components/ui/button.jsx";
+
+function MyComp1({ text }) {
+  const [count, setCount] = useState(0);
+
+  // useEffect의 두번째 파라미터 (배열 [])은
+  // useEffect의 첫번째 파라미터(함수)의 실행을 트리거하는 값들의 목록
+  useEffect(() => {
+    console.log("text나 count가 변경될 때 마다");
+  }, [text, count]);
+
+  useEffect(() => {
+    console.log("text 변경됨");
+  }, [text]);
+
+  useEffect(() => {
+    console.log("count 변경됨");
+  }, [count]);
+
+  useEffect(() => {
+    // mount(initial render)
+    console.log("mount");
+
+    return () => {
+      // unmount
+      console.log("unmount@");
+    };
+  }, []);
+  // mount, update
+  console.log("render...");
+
+  return (
+    <Box>
+      <Button onClick={() => setCount(count + 1)}>count : {count}</Button>
+      <p>text : {text}</p>
+    </Box>
+  );
+}
+function MyApp54(props) {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <Input onChange={(e) => setText(e.target.value)} value={text} />
+      <MyComp1 text={text} />
+    </div>
+  );
+}
+
+export default MyApp54;
