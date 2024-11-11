44a60579daba010e89d97432aa1e48c4269feb79
soomni01 <dsgh8933@naver.com>
2024-11-11 16:36:32 +0900

    post 요청 보내기 (w/ json)

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import App60 from "./practice/App60.jsx";
+import MyApp61 from "./apps/MyApp61.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App60 />
+      <MyApp61 />
       <Toaster />
     </Provider>
   </StrictMode>,
