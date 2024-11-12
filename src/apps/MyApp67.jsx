6bd2d4d02072bae305b89447a0ce7a08c8692822
soomni01 <dsgh8933@naver.com>
2024-11-12 16:39:33 +0900

    useEffect, cleanup 함수 사용

--- src/apps/MyApp67.jsx
+++ src/apps/MyApp67.jsx
@@ -1,68 +1,78 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Field } from "../components/ui/field.jsx";
 import {
   NumberInputField,
   NumberInputRoot,
 } from "../components/ui/number-input.jsx";
-import { Button } from "../components/ui/button.jsx";
-import axios from "axios";
 import { Box, Input } from "@chakra-ui/react";
 
 function MyApp67(props) {
-  const [supplier, setSupplier] = useState(null);
-  const [supplierId, setSupplierId] = useState(0);
+  const [customerId, setCustomerId] = useState(0);
+  const [customer, setCustomer] = useState(null);
 
-  function handleButtonClick() {
+  useEffect(() => {
+    const controller = new AbortController();
+
     axios
-      .get(`/api/main6/sub2/${supplierId}`)
-      .then((res) => setSupplier(res.data));
-  }
+      .get(`/api/main6/sub1/${customerId}`, {
+        signal: controller.signal,
+      })
+      .then((res) => setCustomer(res.data));
+
+    return () => {
+      // 현재 요청 취소코드
+      controller.abort();
+    };
+  }, [customerId]);
 
   return (
     <div>
-      <Field label={"공급자번호"}>
+      <Field label={"고객번호"}>
         <NumberInputRoot
-          value={supplierId}
-          onValueChange={(e) => setSupplierId(e.value)}
+          value={customerId}
+          onValueChange={(e) => setCustomerId(e.value)}
         >
           <NumberInputField />
         </NumberInputRoot>
       </Field>
-      <Button onClick={handleButtonClick}>조회</Button>
       <hr />
-      <SupplierView supplier={supplier} />
+      <CustomerView customer={customer} />
     </div>
   );
 }
-function SupplierView({ supplier }) {
-  if (!supplier) {
-    return <Box>조회된 정보가 없습니다.</Box>;
+
+function CustomerView({ customer }) {
+  if (!customer) {
+    return (
+      <Box>
+        <p>조회된 고객이 없습니다.</p>
+      </Box>
+    );
   }
+
   return (
     <Box>
-      <Field label={"Id"} readOnly>
-        <Input value={supplier.id} />
+      <Field label={"ID"} readOnly>
+        <Input value={customer.id} />
       </Field>
-      <Field label={"name"} readOnly>
-        <Input value={supplier.name} />
+      <Field label={"이름"} readOnly>
+        <Input value={customer.name} />
       </Field>
-      <Field label={"contact"} readOnly>
-        <Input value={supplier.contact} />
+      <Field label={"계약명"} readOnly>
+        <Input value={customer.contact} />
       </Field>
-      <Field label={"address"} readOnly>
-        <Input value={supplier.address} />
+      <Field label={"주소"} readOnly>
+        <Input value={customer.address} />
       </Field>
-      <Field label={"city"} readOnly>
-        <Input value={supplier.city} />
+      <Field label={"도시"} readOnly>
+        <Input value={customer.city} />
       </Field>
-      <Field label={"postalCode"} readOnly>
-        <Input value={supplier.postalCode} />
+      <Field label={"우편번호"} readOnly>
+        <Input value={customer.postalCode} />
       </Field>
-      <Field label={"country"} readOnly>
-        <Input value={supplier.country} />
-      </Field>
-      <Field label={"phone"} readOnly>
-        <Input value={supplier.phone} />
+      <Field label={"국가"} readOnly>
+        <Input value={customer.country} />
       </Field>
     </Box>
   );
