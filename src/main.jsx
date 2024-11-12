5259c02b8bbfd5dbb6e955e173f1765499b0fb98
soomni01 <dsgh8933@naver.com>
2024-11-12 16:24:26 +0900

    공급자 조회

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
 import { Toaster } from "./components/ui/toaster.jsx";
-import MyApp66 from "./apps/MyApp66.jsx";
+import MyApp67 from "./apps/MyApp67.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <MyApp66 />
+      <MyApp67 />
       <Toaster />
     </Provider>
   </StrictMode>,
