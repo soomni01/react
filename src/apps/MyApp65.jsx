9eebbe3bafebd0ddadb065c2f8223e500c884239
soomni01 <dsgh8933@naver.com>
2024-11-12 12:40:07 +0900

    response

--- src/apps/MyApp65.jsx
+++ src/apps/MyApp65.jsx
@@ -8,6 +8,79 @@ function MyApp65(props) {
       <Button
         onClick={() => {
           axios
+            .get("/api/main5/sub7")
+            .then((res) => res.data)
+            .then((d) => {
+              console.log(d.product.name);
+              console.log(d.product.price);
+              console.log(d.product.location[0]);
+              console.log(d.product.location[1]);
+              console.log(d.product.quantity);
+            });
+        }}
+      >
+        btn7
+      </Button>
+      <Button
+        onClick={() => {
+          axios
+            .get("/api/main5/sub6")
+            .then((response) => response.data)
+            .then((data) => {
+              console.log(data.name);
+              console.log(data.items[0]);
+              console.log(data.items[1]);
+              console.log(data.items[2]);
+              console.log(data.team.name);
+              console.log(data.team.location);
+            });
+        }}
+      >
+        btn6
+      </Button>
+      <Button
+        onClick={() => {
+          axios
+            .get("/api/main5/sub5")
+            .then((response) => response.data)
+            .then((book) => {
+              console.log(book);
+              console.log(book.title);
+              console.log(book.content);
+              console.log(book.price);
+              console.log(book.available);
+            });
+        }}
+      >
+        btn5
+      </Button>
+      <Button
+        onClick={() => {
+          axios
+            .get("/api/main5/sub4")
+            .then((response) => response.data)
+            .then((data) => {
+              console.log(data);
+              console.log(data.name);
+              console.log(data.age);
+            });
+        }}
+      >
+        btn4
+      </Button>
+      <Button
+        onClick={() => {
+          axios
+            .get("/api/main5/sub3")
+            .then((response) => response.data)
+            .then((data) => console.log(data));
+        }}
+      >
+        btn3
+      </Button>
+      <Button
+        onClick={() => {
+          axios
             .get("/api/main5/sub1")
             .then((response) => response.data)
             .then((data) => console.log(data));
