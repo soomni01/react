d20313c40213167f636ff6893a9036935e81487a
soomni01 <dsgh8933@naver.com>
2024-11-12 15:11:46 +0900

    response

--- src/apps/MyApp65.jsx
+++ src/apps/MyApp65.jsx
@@ -1,10 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../components/ui/button.jsx";
 import axios from "axios";
+import { Table } from "@chakra-ui/react";
 
+function MyBookTableView({ books }) {
+  if (!books || books.length == 0) {
+    return <div>책 목록을 조회해 주세요.</div>;
+  }
+  return (
+    <>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeader>TITLE</Table.ColumnHeader>
+            <Table.ColumnHeader>CONTENT</Table.ColumnHeader>
+            <Table.ColumnHeader>AUTHOR</Table.ColumnHeader>
+            <Table.ColumnHeader>PRICE</Table.ColumnHeader>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {books.map((book) => (
+            <Table.Row>
+              <Table.Cell>{book.title}</Table.Cell>
+              <Table.Cell>{book.content}</Table.Cell>
+              <Table.Cell>{book.author}</Table.Cell>
+              <Table.Cell>{book.price}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </>
+  );
+}
 function MyApp65(props) {
+  const [books, setBooks] = useState([]);
+
   return (
     <div>
+      <MyBookTableView books={books} />
+      <hr />
+      <Button
+        onClick={() => {
+          axios.get("/api/main5/sub10").then((res) => setBooks(res.data));
+        }}
+      >
+        btn11
+      </Button>
+      <Button
+        onClick={() => {
+          axios
+            .get("/api/main5/sub10")
+            .then((res) => res.data)
+            .then((data) => {
+              console.log(data[0].title);
+              console.log(data[0].author);
+              console.log(data[0].content);
+              console.log(data[0].price);
+
+              console.log(data[1].title);
+              console.log(data[1].author);
+              console.log(data[1].content);
+              console.log(data[1].price);
+
+              console.log(data[2].title);
+              console.log(data[2].author);
+              console.log(data[2].content);
+              console.log(data[2].price);
+            });
+        }}
+      >
+        btn10
+      </Button>
       <Button
         onClick={() => {
           axios
@@ -15,7 +81,6 @@ function MyApp65(props) {
               console.log(data.address);
               console.log(data.married);
               console.log(data.items[0]);
-              console.log(data.items[1]);
               console.log(data.items[1]);
               console.log(data.company.name);
               console.log(data.company.location);
@@ -43,8 +108,8 @@ function MyApp65(props) {
             .then((d) => {
               console.log(d.product.name);
               console.log(d.product.price);
-              console.log(d.product.location[0]);
-              console.log(d.product.location[1]);
+              console.log(d.company.location[0]);
+              console.log(d.company.location[1]);
               console.log(d.product.quantity);
             });
         }}
@@ -55,7 +120,7 @@ function MyApp65(props) {
         onClick={() => {
           axios
             .get("/api/main5/sub6")
-            .then((response) => response.data)
+            .then((res) => res.data)
             .then((data) => {
               console.log(data.name);
               console.log(data.items[0]);
@@ -72,9 +137,8 @@ function MyApp65(props) {
         onClick={() => {
           axios
             .get("/api/main5/sub5")
-            .then((response) => response.data)
+            .then((res) => res.data)
             .then((book) => {
-              console.log(book);
               console.log(book.title);
               console.log(book.content);
               console.log(book.price);
@@ -88,11 +152,11 @@ function MyApp65(props) {
         onClick={() => {
           axios
             .get("/api/main5/sub4")
-            .then((response) => response.data)
-            .then((data) => {
-              console.log(data);
-              console.log(data.name);
-              console.log(data.age);
+            .then((r) => r.data)
+            .then((d) => {
+              console.log(d);
+              console.log(d.name);
+              console.log(d.age);
             });
         }}
       >
@@ -102,8 +166,8 @@ function MyApp65(props) {
         onClick={() => {
           axios
             .get("/api/main5/sub3")
-            .then((response) => response.data)
-            .then((data) => console.log(data));
+            .then((r) => r.data)
+            .then((d) => console.log(d));
         }}
       >
         btn3
